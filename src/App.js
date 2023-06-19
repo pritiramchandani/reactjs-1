0f117fca807a8e5e64dd93d8fbd964af6c59b7a5
pritiramchandani <pritiramchandani124@gmail.com>
2023-06-19 15:10:21 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+
+
 function App() {
+ 
+
+  const [hr, setHr] = useState(0);
+  const [min, setMin] = useState(0);
+  const [sec, setSec] = useState(0);
+  function Timerstart(){
+    setInterval(() => {
+      setSec(sec + 1)
+      if (sec==60) {
+        setSec(0);
+        setMin(min + 1);
+        if (min==60) {
+          setMin(0);
+          setHr(hr + 1);
+        }
+      }
+    }, 1000);
+  }
+
+  
+
+
+  // const [variable, function] = useState(variable = value) 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='hr'>
+        {hr}:
+      </div>
+      <div className='min'>
+        {min}:
+      </div>
+      <div className='sec'>
+        {sec}
+      </div>
+      <div className='button'>
+        <button onClick={Timerstart}> Start</button>
+  
+      </div>
     </div>
-  );
-}
 
+  )
+}
 export default App;
